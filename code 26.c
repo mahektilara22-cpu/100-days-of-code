b5cb12c3a49c310d4e294838787938844532fe53
mahektilara22-cpu <mahektilara22@gmail.com>
2025-10-27 16:35:05 +0530

    Create code 26.c

---
+++ code 26.c
@@ -0,0 +1,16 @@
+//Q26: Write a program to print numbers from 1 to n.
+
+#include <stdio.h>
+int main() {
+    int n, i;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++) {
+        printf("%d", i);
+        if (i < n) {
+            printf(" "); 
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
