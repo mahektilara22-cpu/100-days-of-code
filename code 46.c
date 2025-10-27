c0d98834522aca5599478527de9d00f099277c8a
mahektilara22-cpu <mahektilara22@gmail.com>
2025-10-27 16:51:22 +0530

    Create code 46.c

---
+++ code 46.c
@@ -0,0 +1,20 @@
+//Q46: Write a program to print the following pattern
+//*****
+//*****
+//*****
+//*****
+//***** 
+#include <stdio.h>
+
+int main() {
+	int i,j;
+    for(i = 0; i < 5; i++) {
+        for(j = 0; j < 5; j++) {
+            printf("*");
+        }
+        printf("\n"); // move to next line after each row
+    }
+
+    return 0;
+}
+
