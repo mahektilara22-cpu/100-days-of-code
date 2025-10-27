bac1103aa0bf86e4e49c26f60178e856e00e8903
mahektilara22-cpu <mahektilara22@gmail.com>
2025-10-27 16:43:30 +0530

    Create code 38.c

---
+++ code 38.c
@@ -0,0 +1,17 @@
+//Q38: Write a program to find the sum of digits of a number.
+
+#include <stdio.h>
+int main() {
+    int num, sum = 0, rem;
+    scanf("%d", &num);
+
+    while (num > 0) {
+        rem = num % 10;   
+        sum = sum + rem;  
+        num = num / 10;   
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
